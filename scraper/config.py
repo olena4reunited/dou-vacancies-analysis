40cb5b0e9834d1c9694d62f3af5692af3509ea71
olena4reunited <olenaduringintermission@gmail.com>
2024-11-14 18:34:23 +0200

    init: initialized the project and create config

---
+++ scraper/config.py
@@ -0,0 +1,8 @@
+URLS_EXPERIENCE = [
+    ("https://jobs.dou.ua/vacancies/?category=Python&exp=0-1", "0-1"),
+    ("https://jobs.dou.ua/vacancies/?category=Python&exp=1-3", "1-3"),
+    ("https://jobs.dou.ua/vacancies/?category=Python&exp=3-5", "3-5"),
+    ("https://jobs.dou.ua/vacancies/?category=Python&exp=5plus", "5+")
+]
+
+TECHNOLOGIES = []
